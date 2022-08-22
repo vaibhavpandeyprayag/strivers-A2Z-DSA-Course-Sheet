b7d5e5aa1767185711371f6fe5344d0a150b9a9b
Vaibhav Pandey <83752539+vaibhavpandeyprayag@users.noreply.github.com>
2022-08-22 22:58:03 +0530

    Create pattern9.cpp

---
+++ Learn the basics/Build-up Logical Thinking/pattern9.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void fasterIO() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+}
+
+void solve() {
+    int n = 5;
+
+    for (int i = 0; i <= n; i++) {
+        for (int j = 0; j < n - i; j++)
+            cout << " ";
+        for (int j = 0; j < 2 * i - 1; j++)
+            cout << "*";
+        cout << "\n";
+    }
+    for (int i = n; i >= 0; i--) {
+        for (int j = 0; j < n - i; j++)
+            cout << " ";
+        for (int j = 2 * i - 1; j > 0; j--)
+            cout << "*";
+        cout << "\n";
+    }
+}
+
+int main() {
+    // int tc;
+    // cin >> tc;
+
+    // while (tc--)
+    solve();
+
+    return 0;
+}
