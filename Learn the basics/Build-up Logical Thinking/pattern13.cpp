660f6e99503aecf518bd0c8553cd4caeb5d6ed39
Vaibhav Pandey <83752539+vaibhavpandeyprayag@users.noreply.github.com>
2022-08-24 00:02:02 +0530

    Create pattern13.cpp

---
+++ Learn the basics/Build-up Logical Thinking/pattern13.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void fasterIO() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+}
+
+void solve() {
+    int n = 5;
+    int x = 1;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j <= i; j++)
+            cout << x++ << " ";
+        cout << "\n";
+    }
+}
+
+int main() {
+    // int tc;
+    // cin >> tc;
+
+    // while (tc--)
+    solve();
+
+    return 0;
+}
